4e43c6203e91d67146714d876a71a0c5251a909f
Bùi Công Chuẩn <97036847+BUICONGCHUAN@users.noreply.github.com>
2022-12-01 16:25:14 +0700

    Initial commit

---
+++ scrip.js
@@ -0,0 +1,15 @@
+const imgPosition = document.querySelectorAll(".aspect-ratio-169 img")
+const imgContainer = document.querySelector(".aspect-ratio-169")
+let imgNuber = imgPosition.length
+let index = 0
+
+imgPosition.forEach(function(image,index){
+    image.style.left = index*100+"%"
+})
+function imgSlide(){
+    index++;
+    console.log(index)
+    if(index >= imgNuber) {index = 0}
+    imgContainer.style.left = "_" +index*100 +"%"
+}
+setInterval(imgSlide,5000)
